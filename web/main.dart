112aea9c4e73504ef551446ef96b25197ad2d605
hardhill <hardhill@mail.ru>
2019-12-26 17:08:16 +0800

    design

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,18 @@
 import 'dart:html';
 
+List<Element> boxes;
+
 void main() {
-  
+  boxes = document.querySelectorAll('.box');
+  document.onKeyPress.listen(keyPressed);
+}
+
+void keyPressed(KeyboardEvent event) {
+  if (event.keyCode == KeyCode.LEFT) {
+    boxes[0].style.backgroundColor = '#A33';
+    boxes[1].style.backgroundColor = '';
+  } else if (event.keyCode == KeyCode.RIGHT) {
+    boxes[0].style.backgroundColor = '';
+    boxes[1].style.backgroundColor = '#B33';
+  } else if (event.keyCode == KeyCode.UP || event.keyCode == KeyCode.DOWN) {}
 }
